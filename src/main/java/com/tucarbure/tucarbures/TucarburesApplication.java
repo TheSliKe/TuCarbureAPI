ec568f2bd1df7ff4440b9b31fc132e53a5e4cb49
Julien <juju.dupale@gmail.com>
2022-06-27 22:29:34 +0200

    Wip security bearer

--- src/main/java/com/tucarbure/tucarbures/TucarburesApplication.java
+++ src/main/java/com/tucarbure/tucarbures/TucarburesApplication.java
@@ -1,13 +1,32 @@
 package com.tucarbure.tucarbures;
 
+import com.tucarbure.tucarbures.security.Role;
+import com.tucarbure.tucarbures.security.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TucarburesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TucarburesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+
+		return args -> {
+
+			Role adminRole = roleRepository.findByRole("ADMIN");
+			if (adminRole == null) {
+				Role newAdminRole = new Role();
+				newAdminRole.setRole("ADMIN");
+				roleRepository.save(newAdminRole);
+			}
+		};
+
 	}
 
 }
---
+++ src/main/java/com/tucarbure/tucarbures/security/AuthBody.java
@@ -0,0 +1,10 @@
+package com.tucarbure.tucarbures.security;
+
+import lombok.Data;
+
+@Data
+public class AuthBody {
+
+    private String email;
+    private String password;
+}
