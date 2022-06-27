2ee36636e47a14058951ca1050a285811dca88f2
Julien <juju.dupale@gmail.com>
2022-06-27 08:57:32 +0200

    git init

---
+++ src/main/java/com/tucarbure/tucarbures/TucarburesApplication.java
@@ -0,0 +1,13 @@
+package com.tucarbure.tucarbures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucarburesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TucarburesApplication.class, args);
+	}
+
+}
